9a2eb33cf4d21183a279061f7d3054d9bfce4dd7
sangeeth012 <sangeethra1997@gmail.com>
2025-09-02 17:46:01 +0530

    game

---
+++ Day-6-Assigement.py
@@ -0,0 +1,15 @@
+# function understanding
+def turn right():
+turn_left()
+turn_left()
+turn left()
+
+while not at goal():
+ if wall_in front() == True:
+  turn left()
+ if wall in front() != True:
+  move()
+  if wall_in_front() != True:
+   turn_right()
+  else:
+   turn_right()
